8a92e32f4fceb08f13bbde3789f819d06ca52bfa
lsantana <lsantana@narwalsistemas.com.br>
2025-03-12 22:01:34 -0300

    Add initial migration

--- Planner.Infra.Data/Data/PlannerDbContext.cs
+++ Planner.Infra.Data/Data/PlannerDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Planner.Domain.Domain.Entities;
+using Planner.Infra.Data.EntityConfiguration;
 
 namespace Planner.Infra.Data.Data;
 
@@ -8,5 +9,12 @@ public class PlannerDbContext : DbContext
     public PlannerDbContext(DbContextOptions<PlannerDbContext> options) : base(options) { }
 
     public DbSet<User> User { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new UserConfiguration());
+
+        base.OnModelCreating(modelBuilder);
+    }
 
 }
---
+++ Planner.Infra.Data/EntityConfiguration/UserConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using Planner.Domain.Domain.Entities;
+
+namespace Planner.Infra.Data.EntityConfiguration;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.ToTable("User");
+
+        builder.HasKey(c => c.Id);
+
+        builder.Property(c => c.Email)
+               .IsRequired();
+
+        builder.HasIndex(c => c.Email)
+               .IsUnique();
+    }
+}
